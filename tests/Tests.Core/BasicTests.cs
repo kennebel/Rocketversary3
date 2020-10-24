2f6f2cffc05bdc320ba51a843750085b34070915
Jeremy A. Cunningham <jac@kennebel.com>
2020-10-24 12:06:07 -0600

    Raw/default set up

---
+++ tests/Tests.Core/BasicTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Tests.Core
+{
+    public class BasicTests
+    {
+        [Fact]
+        public void AlwaysPass()
+        {
+            Assert.True(true);
+        }
+    }
+}
